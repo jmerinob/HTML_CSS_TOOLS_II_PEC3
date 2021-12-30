2e2a28e3f607f8b4b3cd63a7f3ff2fa546b18be4
Juan Alfonso Merino <jmerinob@uoc.edu>
2021-12-30 09:52:42 +0100

    _

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backgroundImage: {
+      'zaho-back': "url('../images/quanzhou_06-3.jpg')",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ["./src/**/*.html"]
+};
